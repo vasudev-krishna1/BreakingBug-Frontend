ba1a21734a70986cb2d502535da314058ed5dff8
Dev Grover <Devgrover72@gmail.com>
2024-08-03 15:02:50 +0530

    Update store.js

--- src/redux/store.js
+++ src/redux/store.js
@@ -2,7 +2,8 @@ import { configureStore } from '@reduxjs/toolkit';
 import { userReducer } from './userSlice';
 
 const store = configureStore({
-    reducer={
+    //SYNTAX ERROR
+    reducer:{
         user: userReducer,
     }
 });
